18c36cb8d6934b8615b5e210a49487a09e053723
syhuyit <nguyensyhuy06122005@gmail.com>
2026-03-20 22:06:19 +0700

    finish moviepage

--- js/data.js
+++ js/data.js
@@ -5,8 +5,9 @@ const movies = [
     price: 50000,
     type: "Hành động",
     image: "/images/avengerss.jpeg",
-    description: "Siêu anh hùng",
-    showtime: ["9:00", "20:00"],
+    description:
+      "Avengers Assemble là một loạt phim hoạt hình siêu anh hùng của Mỹ dựa trên nhóm siêu anh hùng Avengers của Marvel Comics. Được thiết kế để tận dụng thành công của bộ phim The Avengers năm 2012, loạt phim này đã được công chiếu trên Disney XD vào ngày 26 tháng 5 năm 2013, là phần tiếp theo của The Avengers: Earth's Mightiest Heroes. Trước đó, phim đã được phát sóng cùng với Ultimate Spider-Man và Guardians of the Galaxy như một phần của chương trình Marvel Universe. Joe Casey, Joe Kelly, Duncan Rouleau và Steven T. Seagle, được biết đến với tên gọi chung là Man of Action, đã phát triển loạt phim và là nhà sản xuất điều hành của mùa 1 và 2.",
+    showtimes: ["9:00", "20:00"],
     status: "trending",
   },
   {
@@ -15,8 +16,9 @@ const movies = [
     price: 40000,
     type: "Tình cảm",
     image: "/images/titanic.webp",
-    description: "Chuyện tình",
-    showtime: ["8:00", "21:00"],
+    description:
+      "Titanic là một bộ phim sử thi lãng mạn lịch sử thảm họa của Mỹ năm 1997 do James Cameron viết kịch bản và đạo diễn. Kết hợp cả yếu tố lịch sử và hư cấu, phim dựa trên những ghi chép về vụ chìm tàu ​​RMS Titanic năm 1912. Leonardo DiCaprio và Kate Winslet đóng vai hai người thuộc hai tầng lớp xã hội khác nhau, yêu nhau trong chuyến hải hành định mệnh đầu tiên của con tàu. Dàn diễn viên còn có Billy Zane, Kathy Bates, Frances Fisher, Bernard Hill, Jonathan Hyde, Danny Nucci, David Warner và Bill Paxton.",
+    showtimes: ["8:00", "21:00"],
     status: "upcoming",
   },
   {
@@ -25,8 +27,9 @@ const movies = [
     price: 70000,
     type: "Hành động",
     image: "/images/godzivskong.jpg",
-    description: "Quái vật",
-    showtime: ["12:00"],
+    description:
+      "Godzilla vs. Kong là một bộ phim quái vật Mỹ năm 2021 do Adam Wingard đạo diễn. Được sản xuất bởi Legendary Pictures và phân phối bởi Warner Bros. Pictures, đây là phần tiếp theo của Kong: Skull Island (2017) và Godzilla: King of the Monsters (2019), và là bộ phim thứ tư trong Vũ trụ Quái vật (Monsterverse). Đây cũng là bộ phim thứ 36 trong loạt phim Godzilla, bộ phim thứ 12 trong loạt phim King Kong, và là bộ phim Godzilla thứ tư được sản xuất hoàn toàn bởi một hãng phim Mỹ. Phim có sự tham gia của các diễn viên Alexander Skarsgård, Millie Bobby Brown, Rebecca Hall, Brian Tyree Henry, Shun Oguri, Eiza González, Julian Dennison, Lance Reddick, Kyle Chandler và Demián Bichir. Brown và Chandler tiếp tục đảm nhận vai diễn của họ từ bộ phim Godzilla trước đó. Trong phim, Kong đụng độ với Godzilla sau khi tổ chức Monarch di chuyển con khỉ đột từ Đảo Skull đến Trái Đất Rỗng, quê hương của những quái vật được gọi là Titan, để lấy nguồn năng lượng cho một vũ khí bí mật nhằm ngăn chặn các cuộc tấn công bí ẩn của Godzilla.",
+    showtimes: ["12:00"],
     status: "trending",
   },
 ];
---
+++ js/movie.js
@@ -0,0 +1,67 @@
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+
+const movie = movies.find((m) => m.id == id);
+
+if (!movie) {
+  alert("Không tìm thấy phim!");
+  throw new Error("Movie not found");
+}
+
+document.getElementById("name").innerText = movie.name;
+document.getElementById("img").src = movie.image;
+document.getElementById("desc").innerText = movie.description;
+document.getElementById("price").innerText = "Giá vé: " + movie.price + " VND";
+
+const timeBox = document.getElementById("times");
+
+let selectedTime = "";
+
+movie.showtimes.forEach((t) => {
+  timeBox.innerHTML += `
+    <button class="btn btn-outline-light m-1" onclick="selectTime('${t}', this)">
+      ${t}
+    </button>`;
+});
+
+function selectTime(t, el) {
+  selectedTime = t;
+
+  document.querySelectorAll("#times button").forEach((btn) => {
+    btn.classList.remove("btn-warning");
+    btn.classList.add("btn-outline-light");
+  });
+
+  el.classList.remove("btn-outline-light");
+  el.classList.add("btn-warning");
+}
+
+function calculate() {
+  const seat = Number(document.getElementById("seat").value);
+  const quantity = Number(document.getElementById("quantity").value);
+
+  const total = movie.price * seat * quantity;
+
+  document.getElementById("total").innerText =
+    "Tổng: " + total.toLocaleString() + " VND";
+  if (total < 0) {
+    alert("Lỗi");
+  }
+}
+
+function addToCart() {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  const total = document.getElementById("total").innerText;
+
+  cart.push({
+    name: movie.name,
+    time: selectedTime,
+    total: total,
+  });
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+
+  alert("Đã thêm vào giỏ vé");
+}
+calculate();
