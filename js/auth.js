9a3a05a10c95ccd6d207d0ccfda2c5c5c09b2845
longdah <long290520041@gmail.com>
2026-04-10 22:36:47 +0700

    fix auth UI

--- js/auth.js
+++ js/auth.js
@@ -4,14 +4,14 @@ form.addEventListener("submit", function (event) {
   event.preventDefault();
 
   const username = document.getElementById("user").value;
-  const password = document.getElementById("password").value;
+  const password = document.getElementById("pass").value;
 
   const aUser = "admin1";
   const aPassword = "123456";
 
   if (aUser == username && aPassword == password) {
     alert("Đăng nhập thành công");
-    window.location.href = "home.html";
+    window.location.href = "pages/home.html";
   } else {
     alert("Not found account");
   }
